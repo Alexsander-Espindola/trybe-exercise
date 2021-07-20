785966aee5a0cd146131a4d152bddff4fa0d6e47
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-07-20 18:07:17 -0300

    bloco-10 e lesson54

--- Fundametos/bloco-10/excercise.test.js
+++ Fundametos/bloco-10/excercise.test.js
@@ -12,16 +12,19 @@ describe('Excercise 1', () => {
 describe('Excercise 2-3', () => {
 	it('should return name from id', () => {
 		expect.assertions(1);
+		
 		return getUserName(4)
 			.then(data => expect(data)
 				.toEqual('Mark'));
 	});
-
+	
 	it('should return an erro', () => {
 		const expected = {
 			error: 'User with 99 not found.'
 		}
+
 		expect.assertions(1);
+
 		return getUserName(99)
 			.then(data => expect(data)
 				.toEqual(expected));
